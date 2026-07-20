91faea1b6cadc957141ef3511e1611c243f8459f
Prasanya20 <prasanya.medapati2006@gmail.com>
2026-07-20 14:27:24 +0530

    Add files via upload

---
+++ Exercise-3/src/main/java/LoggingAspect.java
@@ -0,0 +1,19 @@
+package com.library.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+// Aspect to log method execution times (Exercise 3)
+@Aspect
+public class LoggingAspect {
+
+    @Around("execution(* com.library.service.*.*(..))")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object result = joinPoint.proceed();
+        long elapsed = System.currentTimeMillis() - start;
+        System.out.println(joinPoint.getSignature() + " executed in " + elapsed + " ms");
+        return result;
+    }
+}
